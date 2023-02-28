7a06ca49fd431e2ad6e7126af9114f0188d6534d
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-02-28 13:17:57 -0300

    update: user added into request type

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,9 +1,18 @@
 declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      PGPORT: number | undefined;
+  namespace Express {
+    interface Request {
+      user: {
+        id: string;
+        email: string;
+      };
     }
   }
 }
+// namespace NodeJS {
+//
+// interface ProcessEnv {
+// PGPORT: number | undefined;
+// }
+// }
 
 export {};
