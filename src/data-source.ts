c819e9672e8434ee4cf2ea15190b0250429eddc7
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-02-28 13:20:24 -0300

    updated: changed db port to default

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,8 @@ import "dotenv/config";
 const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
-  port: process.env.PGPORT,
+  // port: process.env.PGPORT,
+  port: 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
