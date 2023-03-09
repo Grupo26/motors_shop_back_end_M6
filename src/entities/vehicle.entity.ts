ade4dcfdd21753dc08852f8c5c42ce3e70ec0092
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-03-08 21:59:59 -0300

    update: alteração nas relações das entities imageGalery e vehicle

--- src/entities/vehicle.entity.ts
+++ src/entities/vehicle.entity.ts
@@ -1,51 +1,50 @@
 import {
-  Column,
-  Entity,
-  ManyToOne,
-  OneToMany,
-  PrimaryGeneratedColumn,
+    Column,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryGeneratedColumn,
 } from "typeorm";
 import { Comment } from "./comments.entity";
 import { ImageGalery } from "./imageGalery.entity";
 import { User } from "./user.entity";
 
-enum TypeVehicle {
-  MOTORCYCLE = "motorcycle",
-  CAR = "car",
-}
-
 @Entity("vehicles")
 export class Vehicle {
-  @PrimaryGeneratedColumn("uuid")
-  readonly id: string;
+    @PrimaryGeneratedColumn("uuid")
+    readonly id: string;
 
-  @Column({ type: "enum", enum: TypeVehicle })
-  type: TypeVehicle;
+    @Column()
+    type: string;
 
-  @Column({ length: 250 })
-  imgCap: string;
+    @Column({ nullable: true })
+    adType: string;
 
-  @Column({ length: 60 })
-  title: string;
+    @Column({ length: 60 })
+    title: string;
 
-  @Column({ length: 2000 })
-  description: string;
+    @Column({ length: 2000 })
+    description: string;
 
-  @Column({ type: "decimal" })
-  km: number;
+    @Column({ type: "decimal" })
+    km: number;
 
-  @Column()
-  year: number;
+    @Column()
+    year: number;
 
-  @Column({ type: "decimal" })
-  value: number;
+    @Column({ type: "decimal" })
+    value: number;
 
-  @ManyToOne(() => User, (user) => user.vehicles)
-  users: User;
+    @OneToOne(() => ImageGalery, { eager: true, onDelete: "CASCADE" })
+    @JoinColumn()
+    imageGalery: ImageGalery;
 
-  @OneToMany(() => ImageGalery, (image_galeries) => image_galeries.vehicles)
-  imageGaleries: ImageGalery[];
+    @ManyToOne(() => User, (user) => user.vehicles)
+    users: User;
 
-  @OneToMany(() => Comment, (comment) => comment.vehicles)
-  comments: Comment[];
+    @OneToMany(() => Comment, (comment) => comment.vehicles)
+    comments: Comment[];
 }
+
