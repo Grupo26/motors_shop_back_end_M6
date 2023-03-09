81043a1283b05c9d2142e675950a3e68b2066382
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-03-08 22:02:13 -0300

    update: alteração no controller de criação de vehicle

--- src/controllers/vehicles/register.ts
+++ src/controllers/vehicles/register.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
 import { registerVehicleService } from "../../services/vehicles/register";
+import { uploadImageService } from "../../services/photos/uploader";
 
 const registerVehicleController = async (req: Request, res: Response) => {
-  const data = req.body;
-  const userId = req.user.id;
-  const vehicle = await registerVehicleService(data, userId);
-  return res.status(201).json(vehicle);
+    const files = req.files as Express.Multer.File[];
+    const data = req.body;
+    const userId = req.user.id;
+
+    if (!req.files)
+        return res.status(400).json({ message: "No files uploaded" });
+
+    const photos = await uploadImageService(files);
+
+    const vehicle = await registerVehicleService(data, photos, userId);
+    return res.status(201).json(vehicle);
 };
 export { registerVehicleController };
+
