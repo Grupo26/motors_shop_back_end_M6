7dc1d229f57b5f01453d502fc8f8c28e7e892163
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-03-08 22:24:15 -0300

    feat: criação de controller e route to upload imagens

--- src/routes/images.route.ts
+++ src/routes/images.route.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { deleteImageController } from "../controllers/photos/destroy";
+import { uploadImageController } from "../controllers/photos/upload";
 import ensureAuthMiddleware from "../middleware/ensureAuth.middleware";
+import uploadImageMiddleware from "../middleware/uploadImage.middlewares";
 
 const imagesRouter = Router();
 
+imagesRouter.post(
+    "",
+    ensureAuthMiddleware,
+    uploadImageMiddleware.array("imageGalery"),
+    uploadImageController
+);
 imagesRouter.delete("/:id", ensureAuthMiddleware, deleteImageController);
-
 export default imagesRouter;
 
